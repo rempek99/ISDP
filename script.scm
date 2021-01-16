1cc71d64bd6f4a85da7abef5a6644370e85fd437
rempek99 <arkadiusz.remplewicz@gmail.com>
2021-01-16 19:49:47 +0100

    s

--- script.scm
+++ script.scm
@@ -22,7 +22,8 @@ pipeline {
 			
              sh "whoami" 
 		//Running & deploy on Payara
-        	sh "/payara/payara5.2020.5/bin/asadmin -u admin start-domain"
+        	sh "/payara/payara5.2020.5/bin/asadmin start-domain"
+            sh 'sleep 5000'
 		    sh "/payara/payara5.2020.5/bin/asadmin -u admin deploy --force /var/lib/jenkins/workspace/ISDP/WM/target/WM.war"		
 
 }

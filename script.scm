39694e25d1e5ac1d9ff19539639e9c39a4ae2a00
rempek99 <arkadiusz.remplewicz@gmail.com>
2021-01-16 17:59:55 +0100

    Script file

---
+++ script.scm
@@ -0,0 +1,21 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Build') {
+            steps {
+                // Get some code from a GitHub repository
+                git 'https://github.com/rempek99/ISDP.git'
+
+                // Run Maven on a Unix agent.
+                sh "mvn -Dmaven.test.failure.ignore=true -f WM/pom.xml clean package"
+
+                // To run Maven on a Windows agent, use
+                // bat "mvn -Dmaven.test.failure.ignore=true clean package"
+            }
+
+            
+            }
+        }
+    
+}

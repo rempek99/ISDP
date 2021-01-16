4fb71164a7e5769e5a9835bf7d673d15523babe6
rempek99 <arkadiusz.remplewicz@gmail.com>
2021-01-16 18:58:52 +0100

    s

--- script.scm
+++ script.scm
@@ -20,9 +20,11 @@ pipeline {
 		//Running JDB
 		sh "/home/student/JavaTools/db-derby-10.14.2.0-bin/bin/ij connect 'jdbc:derby://localhost:1527/WM;create=true;user=WM;password=WM';"
 			
+             sh "whoami" 
 		//Running & deploy on Payara
             	sh "/payara/payara5.2020.5/bin/asadmin start-domain"
-		sh "/payara/payara5.2020.5/bin/asadmin deploy --force /var/lib/jenkins/workspace/ISDP/WM/target/WM.war"		 
+		sh "/payara/payara5.2020.5/bin/asadmin deploy --force /var/lib/jenkins/workspace/ISDP/WM/target/WM.war"		
+
 }
 
             

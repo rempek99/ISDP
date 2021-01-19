2c5c172f135a5bec49b4c56cc922bfa3965a2575
Arkadiusz Remplewicz <58391537+rempek99@users.noreply.github.com>
2021-01-19 21:59:05 +0100

    Update NewAccountTest.java

--- Tests/src/test/java/NewAccountTest.java
+++ Tests/src/test/java/NewAccountTest.java
@@ -22,7 +22,7 @@ import org.openqa.selenium.Keys;
 import java.util.*;
 import java.net.MalformedURLException;
 import java.net.URL;
-public class Test1Test {
+public class NewAccountTest {
   private WebDriver driver;
   private Map<String, Object> vars;
   JavascriptExecutor js;

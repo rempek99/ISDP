dadfa4ec2296502d454a86c828db789f0df90534
Paweł Bucki <mrpawrob@gmail.com>
2021-01-19 19:55:26 +0100

    Update script.scm

--- script.scm
+++ script.scm
@@ -25,7 +25,7 @@ pipeline {
 
                 sh "/payara/payara5.2020.5/bin/asadmin start-domain"
                 sh "/payara/payara5.2020.5/bin/asadmin -u admin deploy --force /var/lib/jenkins/workspace/ISDP/WM/target/WM.war"
-                //sh "mvn -Dmaven.test.failure.ignore=true -f Tests/pom.xml test"
+                sh "mvn -Dmaven.test.failure.ignore=true -f Tests/pom.xml test"
                 sh '/payara/payara5.2020.5/bin/asadmin stop-domain'
 
                     //sh "/home/student/JavaTools/db-derby-10.14.2.0-bin/bn/stopNetworkServer"
